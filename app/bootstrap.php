90bfc5b20f9ea8850b9ade300553101608e058e6
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-06-13 23:40:27 +0100

    create model user and function resigter in model and function rederict to login after register

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 // Load Config
 require_once 'config/config.php';
-
+// Load helpers
+require_once 'helpers/url_hepler.php';
 // Autoload Core Libraries
 spl_autoload_register(function ($className) {
   require_once 'libraries/' . $className . '.php';
