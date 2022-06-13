34a7be8378b49944ee8364a545da5ec29dd4122a
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-06-13 21:06:12 +0100

    finished controller login and register

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -7,19 +7,20 @@
         <div>
           <h2 id="text_login">Login</h2>
         </div>
-        <form>
+        <form action="<?php echo URLROOT; ?>/users/login" method="POST">
           <!-- Email input -->
           <div class="form-outline mb-4">
-            <input type="email" placeholder="Email" name="email"
-              class="form-control <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"
+            <input type="email" name="email" placeholder="Email"
+              class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"
               value="<?php echo $data['email']; ?>" />
+            <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
           </div>
           <!-- Password input -->
           <div class="form-outline mb-4">
-            <input type="password" placeholder="Password" name="password"
-              class="form-control <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"
-              value="<?php echo $data['password']; ?>" />
-            <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+            <input type="password" name="password"
+              class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"
+              value="<?php echo $data['password']; ?>/>
+            <span class=" invalid-feedback"><?php echo $data['password_err']; ?></span>
           </div>
           <!-- 2 column grid layout for inline styling -->
           <div class="row mb-4">
@@ -32,7 +33,7 @@
             </div>
           </div>
           <!-- Submit button -->
-          <button type="button" id="login" class="btn form-control  mb-4">Log in</button>
+          <button type="submit" id="login" class="btn form-control  mb-4">Log in</button>
           <!-- Register buttons -->
           <div class="text-start">
             <p class="text-dark fst-italic"><small> If don't have accout <a href="<?= URLROOT ?>/users/register"
