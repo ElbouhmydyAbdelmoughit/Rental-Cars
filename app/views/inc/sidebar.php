c6c701842b7f955fac9b062e2e67d84310234f43
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-06-15 16:24:50 +0100

    solved problem icons sidebar

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -1,20 +1,29 @@
 <div class="bg-white" id="sidebar">
   <ul>
-    <li class="mb-5"><a href="#"></a>
-      <i class="bi bi-house-door-fill"></i>
+    <li class="mb-5">
+      <a href="<?= URLROOT ?>">
+        <i class="bi bi-house-door-fill"></i>
+      </a>
     </li>
-    <li class="mb-5"><a href="#"></a>
-      <i class="fa-solid fa-car"></i>
+    <li class="mb-5">
+      <a href="<?= URLROOT ?>" data-bs-toggle="tooltip" data-bs-placement="right" title="Home">
+        <i class="fa-solid fa-car"></i>
+      </a>
     </li>
-    <li class="mb-5"><a href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Client"></a>
-      <i class="fa-solid fa-user"></i>
+    <li class="mb-5">
+      <a href="<?= URLROOT ?>" data-bs-toggle="tooltip" data-bs-placement="right" title="Client">
+        <i class="fa-solid fa-user"></i>
+      </a>
     </li>
-    <li class="mb-5"><a href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Admin"></a>
-      <i class="fa-solid fa-users-gear"></i>
+    <li class="mb-5">
+      <a href="<?= URLROOT ?>" data-bs-toggle="tooltip" data-bs-placement="right" title="Admin">
+        <i class="fa-solid fa-users-gear"></i>
+      </a>
     </li>
-    <li id="logout" class="mb-5"><a href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Logout"
-        style="color: red;"></a>
-      <i class="fa-solid fa-right-from-bracket"></i>
+    <li id="logout" class="mb-5">
+      <a href="<?= URLROOT ?>" data-bs-toggle="tooltip" data-bs-placement="right" title="Logout" style="color: red;">
+        <i class="fa-solid fa-right-from-bracket"></i>
+      </a>
     </li>
   </ul>
 </div>
