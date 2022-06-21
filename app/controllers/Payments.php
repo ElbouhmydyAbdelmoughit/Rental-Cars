edb4c7f3f4f79ce42040f951f58219a4ff73b6a0
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-06-21 17:59:08 +0100

    create model and controller payment

--- app/controllers/Payments.php
+++ app/controllers/Payments.php
@@ -1,8 +1,37 @@
 <?php
-class Payment extends Controller
+class Payments extends Controller
 {
-  private $db;
   public function __construct()
   {
+    if (!isLoggedIn()) {
+      redirect('users/login');
+    }
+    $this->carModel = $this->model('Car');
+    $this->paymentModel = $this->model('Payment');
+  }
+
+
+  public function Reserve($id)
+  {
+    $car = $this->carModel->getCarById($id);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+      echo '<pre>';
+      print_r($_POST);
+      echo '<pre>';
+      exit;
+      $data = [
+        'car' => $car
+      ];
+
+      $this->paymentModel->create_payment();
+      $this->view('pages/payment', $data);
+    } else {
+      $data = [
+        'car' => $car,
+      ];
+
+      $this->view('pages/payment', $data);
+    }
   }
 }
---
+++ app/models/Payment.php
@@ -0,0 +1,13 @@
+<?php
+
+class Payment
+{
+  public function __construct()
+  {
+    $this->db = new Database;
+  }
+
+  public function create_payment()
+  {
+  }
+}
