e2d4baf2d2428f2866b5b2c4831207cc0df3c7ca
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-06-23 13:47:59 +0100

    finished design pages UpdateCar and AddCar

--- app/views/cars/index.php
+++ app/views/cars/index.php
@@ -1,21 +1,21 @@
 <div class="bg-light" id="dashboard">
   <div class="row col-12">
-    <div class="contant d-flex">
-      <div class="col-2 me-3">
+    <div class="contant d-flex mx-auto justify-content-center">
+      <div class="">
         <?php require APPROOT . '/views/inc/header_sidebar.php'; ?>
       </div>
-      <div class="col-10 mt-5 table-responsive">
+      <div class=" mt-5 table-responsive mx-auto">
         <div class="col-6 text-center mx-auto my-5" role="alert">
           <?php flash('car_message'); ?>
         </div>
-        <table class="table border border-1 border-dark text-nowrap" style="min-width: 600px;">
+        <table class="table border-dark text-nowrap table table-dark table-hover" style="width: 750px;">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Model</th>
-              <th scope="col">Name</th>
-              <th scope="col">Price</th>
-              <th scope="col">Operation</th>
+              <th>#</th>
+              <th>Model</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th class="d-flex justify-content-center"> Opreration</th>
             </tr>
           </thead>
           <tbody>
@@ -26,7 +26,7 @@
               <td><?php echo $car->name; ?></td>
               <td><?php echo $car->price; ?></td>
               <td>
-                <div class="operation">
+                <div class="operation d-flex justify-content-center">
                   <a class="me-3 text-success" href="<?php echo URLROOT; ?>/cars/update/<?= $car->id ?>"><i
                       class="bi bi-pen"></i></a>
                   <a class="ms-3 text-danger" href="<?php echo URLROOT; ?>/cars/delete/<?= $car->id ?>"><i
